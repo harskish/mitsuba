5917d344ab8dd3153bb9ea79833f986ba9ace4e4
Erik Härkönen <erik.harkonen@aalto.fi>
2017-05-26 13:32:39 +0300

    Servers killed on render completion

--- scripts/mitsuba_task_single.sh
+++ scripts/mitsuba_task_single.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+
+SCENE=$HOME/scenes/veach-bidir-mlt/veach-bidir-mlt.xml
+
+
 # Get a list of hosts using python-hostlist
 nodes=`hostlist --expand $SLURM_NODELIST|xargs`
  
@@ -18,14 +22,26 @@ localid=$SLURM_LOCALID
 # Join hostnames with semicolons
 workerlist=${workers// /;}
 
+# Save output to logs folder
+cd ~/mitsuba/logs
+
 if [[ "$me" == "$master" && "$localid" -eq 0 ]]
 then
    # Master
    sleep 5
-   command="mitsuba $HOME/scenes/veach-bidir-mlt/veach-bidir-mlt.xml -c '$workerlist'"
+   command="mitsuba $SCENE -c '$workerlist'"
    echo "Master comand: $command"
+   
    eval $command
+
+   # Kill servers to complete job
+   for srv in $workers
+   do
+      kill_command="ssh $srv 'pkill -U $USER mtssrv'"
+      echo "Running command $kill_command"
+      eval $kill_command
+   done
 else
    # Worker
-   mtssrv
+   mtssrv -q
 fi
